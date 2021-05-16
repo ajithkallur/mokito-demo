351f12fdda8de6cad7843c847f4adca9e8cfc52b
Ajith.KumarReddy <Ajith.KumarReddy@IN2270990W1.ey.net>
2021-05-16 22:10:47 +0530

    Mokito-demo working

---
+++ mokito-demo/src/main/java/com/in28minutes/mockito/mockitodemo/MokitoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.mockito.mockitodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokitoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokitoDemoApplication.class, args);
+	}
+
+}
